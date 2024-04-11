ef94c55b72d9f5c3202de07ec5ecbd89834d59ed
H <hien.lequang47@hcmut.edu.vn>
2024-04-11 16:58:29 +0700

    fetch led and lux data

--- be/adafruit.js
+++ be/adafruit.js
@@ -5,6 +5,8 @@ const pool = db_config;
 const ADAFRUIT_IO_KEY = "aio_kTbb73aaRft4oVeofa62LY3IDxro";
 const ADAFRUIT_IO_USERNAME = "duongwt16";
 const FEED_NAME = "temp";
+const LED_FEED_NAME = "led";
+const LUX_FEED_NAME = "lux";
 
 pool
   .connect()
@@ -51,7 +53,93 @@ pool
       }
     };
 
+    const fetchLedDataAndPrint = async () => {
+      try {
+        const ledResponse = await axios.get(
+          `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${LED_FEED_NAME}/data`,
+          {
+            headers: {
+              "X-AIO-Key": ADAFRUIT_IO_KEY,
+            },
+          }
+        );
+        const ledData = ledResponse.data;
+        if (ledData.length > 0) {
+          const latestData = ledData[0];
+          const status = latestData.value;
+          const recordDate = new Date(latestData.created_at);
+          const newActivityDescription = status == 0 ? "OFF" : "ON";
+          // Get the status of the led from the activity table where device_id = L1
+          const latestLedActivity = await pool.query(
+            "SELECT activity_description FROM activity WHERE device_id = 'L1' ORDER BY activity_id DESC LIMIT 1"
+          );
+          const latestActivityDescription = latestLedActivity.rows[0].activity_description || "OFF"; // If no records, set it to "OFF"
+
+          if (latestActivityDescription != newActivityDescription){
+            // Get the latest activity_id from the activity table
+            const latestActivity = await pool.query(
+              "SELECT MAX(activity_id) AS max_id FROM activity"
+            );
+            const latestActivityId = latestActivity.rows[0].max_id || 0; // If no records, set it to 0
+            const newActivityId = latestActivityId + 1; // Generate a new activity_id
+
+            await pool.query(
+              "INSERT INTO activity (activity_id, activity_time, activity_description, device_id) VALUES ($1, $2, $3, 'L1')",
+              [newActivityId, recordDate, newActivityDescription]
+            );
+            console.log(`Latest led status: ${newActivityDescription} at ${recordDate}`);
+          }
+        } else {
+          console.log("No led data available from Adafruit");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    const fetchLuxDataAndPrint = async () => {
+      try {
+        const luxResponse = await axios.get(
+          `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${LUX_FEED_NAME}/data`,
+          {
+            headers: {
+              "X-AIO-Key": ADAFRUIT_IO_KEY,
+            },
+          }
+        );
+        const luxData = luxResponse.data;
+        if (luxData.length > 0) {
+          const latestData = luxData[0];
+          const lux = latestData.value;
+          const recordDate = new Date(latestData.created_at);
+
+          // Get the latest record_id from the record table
+          const latestRecord = await pool.query(
+            "SELECT MAX(record_id) AS max_id FROM record"
+          );
+          const latestRecordId = latestRecord.rows[0].max_id || 0; // If no records, set it to 0
+          const newRecordId = latestRecordId + 1; // Generate a new record_id
+
+          await pool.query(
+            "INSERT INTO record (record_id, record_date, device_id) VALUES ($1, $2, 2)",
+            [newRecordId, recordDate]
+          );
+          await pool.query(
+            "INSERT INTO lux_record (lux_record_id, lux) VALUES ($1, $2)",
+            [newRecordId, lux]
+          );
+          console.log(`Latest lux: ${lux} at ${recordDate}`);
+        } else {
+          console.log("No lux data available from Adafruit");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
     setInterval(fetchDataAndPrint, 10000);
+    setInterval(fetchLedDataAndPrint, 1000);
+    setInterval(fetchLuxDataAndPrint, 10000);
   })
   .catch((error) => {
     console.error("Error connecting to the database:", error);
