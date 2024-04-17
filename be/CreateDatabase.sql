cc1aafa0b48b8146dfc2b9c35f80c79243f5c342
H <hien.lequang47@hcmut.edu.vn>
2024-04-17 15:57:07 +0700

    update database

--- be/CreateDatabase.sql
+++ be/CreateDatabase.sql
@@ -28,20 +28,20 @@ CREATE TABLE device (
 	username VARCHAR(50)
 );	
 CREATE TABLE record (
-	record_id VARCHAR(50) PRIMARY KEY,
+	record_id INT PRIMARY KEY,
 	record_date DATE NOT NULL,
 	device_id VARCHAR(50) NOT NULL
 );
 CREATE TABLE moisture_record (
-	moisture_record_id VARCHAR(50) PRIMARY KEY,
+	moisture_record_id INT PRIMARY KEY,
 	moisture REAL NOT NULL
 );
 CREATE TABLE temperature_record (
-	temperature_record_id VARCHAR(50) PRIMARY KEY,
+	temperature_record_id INT PRIMARY KEY,
 	temperature REAL NOT NULL
 );
 CREATE TABLE light_record (
-	light_record_id VARCHAR(50) PRIMARY KEY,
+	light_record_id INT PRIMARY KEY,
 	lux REAL NOT NULL
 );
 CREATE TABLE activity (
@@ -74,3 +74,13 @@ ADD FOREIGN KEY (temperature_record_id) REFERENCES record(record_id);
 
 ALTER TABLE light_record
 ADD FOREIGN KEY (light_record_id) REFERENCES record(record_id);
+
+INSERT INTO users (user_name, hashed_password) VALUES ('username1', 'password1');
+
+INSERT INTO device (device_id, device_type, device_location, username) VALUES 
+	('1', 'temp_sensor', 'BK', 'username1'),
+	('2', 'light_sensor', 'BK', 'username1'),
+	('L1', 'light', 'BK', 'username1'),
+	('L2', 'light', 'BK', 'username1'),
+	('L3', 'light', 'BK', 'username1');
+
--- be/adafruit.js
+++ be/adafruit.js
@@ -53,6 +53,8 @@ pool
       }
     };
 
+    let lastLedId = "";
+
     const fetchLedDataAndPrint = async () => {
       try {
         const ledResponse = await axios.get(
@@ -66,26 +68,108 @@ pool
         const ledData = ledResponse.data;
         if (ledData.length > 0) {
           const latestData = ledData[0];
+          const ledId = latestData.id;
           const status = latestData.value;
           const recordDate = new Date(latestData.created_at);
           const newActivityDescription = status == 0 ? "OFF" : "ON";
-          // Get the status of the led from the activity table where device_id = L1
-          const latestLedActivity = await pool.query(
-            "SELECT acttivity_description FROM activity WHERE device_id = 'L1' ORDER BY activity_id DESC LIMIT 1"
-          );
-          const latestActivityDescription = latestLedActivity.rows[0].acttivity_description || "OFF"; // If no records, set it to "OFF"
-
-          if (latestActivityDescription != newActivityDescription){
+          
+          if (ledId !== lastLedId) {
+            lastLedId = ledId;
             await pool.query(
               "INSERT INTO activity (activity_time, acttivity_description, device_id) VALUES ($1, $2, 'L1')",
               [recordDate, newActivityDescription]
             );
             console.log(`Latest led status: ${newActivityDescription} at ${recordDate}`);
-          }
+          }          
         } else {
           console.log("No led data available from Adafruit");
         }
       } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    const setLatestLedId = async () => {
+      try {
+        const response = await axios.get(
+          `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${LED_FEED_NAME}/data`,
+          {
+            headers: {
+              "X-AIO-Key": ADAFRUIT_IO_KEY,
+            },
+          }
+        );
+        const data = response.data;
+        const latestData = data[0];
+        lastLedId = latestData.id;
+      }
+      catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    const postLedData = async (value) => {
+      try {
+        await axios.post(
+          `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${LED_FEED_NAME}/data`,
+          {
+            value: value,
+          },
+          {
+            headers: {
+              "X-AIO-Key": ADAFRUIT_IO_KEY,
+            },
+          }
+        );
+        console.log(`LED turned ${value == 0 ? "OFF" : "ON"}`);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    const sendLedData = async () => {
+      try {
+        const ledCheckResponse = await axios.get(
+          `https://io.adafruit.com/api/v2/${ADAFRUIT_IO_USERNAME}/feeds/${LED_FEED_NAME}/data`,
+          {
+            headers: {
+              "X-AIO-Key": ADAFRUIT_IO_KEY,
+            },
+          }
+        );
+        const ledCheckData = ledCheckResponse.data;
+        if (ledCheckData.length > 0) {
+          const data = ledCheckData[0];
+          const status = data.value;
+          
+          // get the latest led status from the activity table
+          const latestActivity = await pool.query(
+            "SELECT * FROM activity WHERE device_id = 'L1' ORDER BY activity_time DESC LIMIT 1"
+          );
+          const latestActivityDescription = latestActivity.rows[0].acttivity_description;
+
+          if (status == 0 && latestActivityDescription == "ON") {
+            await postLedData(1);
+            await setLatestLedId();
+          } else if (status == 1 && latestActivityDescription == "OFF") {
+            await postLedData(0);
+            await setLatestLedId();
+          }
+        } else {
+          const latestActivity = await pool.query(
+            "SELECT * FROM activity WHERE device_id = 'L1' ORDER BY activity_time DESC LIMIT 1"
+          );
+          const latestActivityDescription = latestActivity.rows[0].acttivity_description;
+          if (latestActivityDescription == "ON") {
+            await postLedData(1);
+            await setLatestLedId();
+          } else if (latestActivityDescription == "OFF") {
+            await postLedData(0);
+            await setLatestLedId();
+          }
+        }
+      }
+      catch (error) {
         console.error("Error:", error);
       }
     };
@@ -111,7 +195,7 @@ pool
             "SELECT MAX(record_id) AS max_id FROM record"
           );
           const latestRecordId = latestRecord.rows[0].max_id || 0; // If no records, set it to 0
-          const newRecordId = parseInt(latestRecordId) + 1; // Generate a new record_id
+          const newRecordId = latestRecordId + 1; // Generate a new record_id
 
           await pool.query(
             "INSERT INTO record (record_id, record_date, device_id) VALUES ($1, $2, 2)",
@@ -130,9 +214,10 @@ pool
       }
     };
 
-    setInterval(fetchDataAndPrint, 10000);
-    setInterval(fetchLedDataAndPrint, 1000);
-    setInterval(fetchLuxDataAndPrint, 10000);
+    // setInterval(fetchDataAndPrint, 10000);
+    // setInterval(fetchLedDataAndPrint, 1000);
+    setInterval(sendLedData, 10000);
+    // setInterval(fetchLuxDataAndPrint, 11000);
   })
   .catch((error) => {
     console.error("Error connecting to the database:", error);
