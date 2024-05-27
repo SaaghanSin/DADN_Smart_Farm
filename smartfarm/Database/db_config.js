5fbba89a2dc8b930b71b255f509a46aadcd59c41
SaaghanSin <96587554+SaaghanSin@users.noreply.github.com>
2024-05-27 15:55:26 +0700

    resolve

--- smartfarm/Database/db_config.js
+++ smartfarm/Database/db_config.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const db_config = new Pool({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "02072003",
-    database: "smartfarm"
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "123456",
+  database: "smartfarm",
 });
 
 module.exports = db_config;
